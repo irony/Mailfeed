153b38f8bb0aaa1b0e116a9f47399aef0bb5f793
irony <cl@iteam.se>
2011-09-27 10:23:34 +0200

    initial commit

---
+++ MailFeed/Controllers/MailController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Dynamic;
+
+namespace MailFeed.Controllers
+{
+    public class MailController : Controller
+    {
+        //
+        // GET: /Mail/
+
+        static MailController()
+        {
+            Inbox = new Mail[0];
+        }
+
+        public static Mail[] Inbox
+        {
+            get;
+            set;
+        }
+
+        public ActionResult Index()
+        {
+            dynamic model = new ExpandoObject();
+            model.Inbox = Inbox;
+
+            return View(model);
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Mail mail)
+        {
+            // TODO: Add to DB
+            List<Mail> mails = new List<Mail>();
+            mails.Add(mail);
+            mails.AddRange(Inbox);
+            Inbox = mails.ToArray();
+
+
+            return new RedirectResult("/Mail");
+        }
+
+    }
+}
---
+++ MailFeed/Models/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MailFeed.Controllers
+{
+    public class Mail
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        /*public [] Attachments { get; set; }*/
+    }
+}
