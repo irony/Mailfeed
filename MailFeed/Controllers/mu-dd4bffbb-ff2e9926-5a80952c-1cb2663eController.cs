f7b5358cd55fcad3be90df3f3ec97faed5397bb4
irony <cl@iteam.se>
2011-09-28 16:46:21 +0200

    blitz authorization

---
+++ MailFeed/Controllers/mu-dd4bffbb-ff2e9926-5a80952c-1cb2663eController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MailFeed.Controllers
+{
+    public class mu_dd4bffbb_ff2e9926_5a80952c_1cb2663eController : Controller
+    {
+        /// <summary>
+        /// This is for authorization for blitz
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return Content("42");
+        }
+
+    }
+}
